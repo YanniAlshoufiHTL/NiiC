a8856cf379b3028d75f804a1d363cee52d5e2e07
iboLawand <l.ibo@HTBLALeonding.onmicrosoft.com>
2024-02-29 14:31:32 +0100

    ok

--- code/database/2024_02_29_create_tables.sql
+++ code/database/2024_02_29_create_tables.sql
@@ -1,24 +1,24 @@
 CREATE TABLE USER
 (
     username TEXT unique PRIMARY KEY
-) STRICT;
+);
 
 CREATE TABLE CALENDER
 (
     id       INTEGER unique PRIMARY KEY,
     username TEXT,
     FOREIGN KEY (username) REFERENCES USER (username)
-) STRICT;
+);
 
 CREATE TABLE AET
 (
     id         INTEGER PRIMARY KEY,
     name       TEXT,
-    begin      DATETIME,
-    end        DATETIME,
+    begin      time,
+    end        time,
     calenderid INTEGER,
     FOREIGN KEY (calenderid) REFERENCES CALENDER (id)
-) STRICT;
+);
 
 CREATE TABLE BlOCKMODULE
 (
@@ -29,26 +29,10 @@ CREATE TABLE BlOCKMODULE
     html        TEXT,
     css         TEXT,
     js          TEXT,
-    FOREIGN KEY (token) REFERENCES validToken (token)
-
+    FOREIGN KEY (token) REFERENCES VALIIDTOKEN (token)
 );
 
 CREATE TABLE VALIIDTOKEN
 (
-    token varchar(20) primary key
-) ;
-
-INSERT INTO USER (username)
-VALUES ('Yanni'),
-       ('Clemens'),
-       ('Lawand'),
-       ('t1'),
-       ('t2'),
-       ('t3');
-INSERT INTO CALENDER (id, username)
-VALUES (1, 'Yanni'),
-       (2, 'Clemens'),
-       (3, 'Lawand'),
-       (4, 't1'),
-       (5, 't2'),
-       (6, 't3');
+    token  varchar(40) primary key
+);
