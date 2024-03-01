49c4b3fda0fc319fe2ac9bcf247cc5dea9ea1b31
iboLawand <l.ibo@HTBLALeonding.onmicrosoft.com>
2024-03-01 12:17:42 +0100

    Documented the code on my own free will,it should be ok now

--- code/database/2024_02_29_create_tables.sql
+++ code/database/2024_02_29_create_tables.sql
@@ -1,39 +1,37 @@
-DROP TABLE valiidtoken;
-CREATE TABLE niiicuser
+
+CREATE TABLE niicuser --- mistake with spelling niicuser, tablename should be in lowercase because sql syntax
 (
-    username TEXT unique PRIMARY KEY
+    id       SERIAL PRIMARY KEY,
+    username TEXT UNIQUE
 );
 
-CREATE TABLE calender
+CREATE TABLE calendar ---had mistake with spelling calendar
 (
-    id       serial,
-    username TEXT,
-    FOREIGN KEY (username) REFERENCES niiicuser (username)
+    id       SERIAL PRIMARY KEY ,
+    username VARCHAR(255),
+    FOREIGN KEY (username) REFERENCES niicuser (username)
 );
 
-CREATE TABLE AET
+CREATE TABLE aet
 (
-    id         serial,
-    name       TEXT,
-    begin      text,
-    enddd      text,
-    calenderid  serial,
-    FOREIGN KEY (calenderid) REFERENCES CALENDER (id)
+    id         SERIAL PRIMARY KEY , -- ids should be primary keys because it is a constraint attribute
+    name       VARCHAR(255),
+    tsbegin    TIMESTAMP,        -- timestamp because tsbegin is the time when the aet begins
+    tsend      TIMESTAMP,        -- the same goes for here tsend is the time when the aet ends
+    calenderid  SERIAL,
+    FOREIGN KEY (calenderid) REFERENCES calendar(id)
 );
 
-CREATE TABLE BlOCKMODULE
+CREATE TABLE blockmodule
 (
-    id          INTEGER primary key,
-    token       TEXT, -- from valid token
-    title       TEXT,
+    id          SERIAL PRIMARY KEY, --Serial because it generates ids automatically(incrementing)
+    token       VARCHAR(40) UNIQUE, -- two blockmodule shouldn't habe the same token
+    title       VARCHAR(255),
     description TEXT,
     html        TEXT,
     css         TEXT,
-    js          TEXT,
-    FOREIGN KEY (token) REFERENCES VALIIDTOKEN (token)
+    js          TEXT
 );
 
-CREATE TABLE VALIIDTOKEN
-(
-    token  varchar(40) primary key
-);
+--We do not need the validtoken table because the token is supposed to be unique
+-- so it would be useless to check it with an extra tble
