d98c5f589003a298d4f6b56058b367dc0e9e7f21
ClemensGugerbauer <c.gugerbauer@HTBLALeonding.onmicrosoft.com>
2024-03-03 13:22:11 +0100

    Finishing project structur

---
+++ code/out/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+app.use(express.json()); // parse JSON data and places result in req.body
+app.listen(3000, function () {
+    console.log("Server listening on port 3000 ...");
+});
