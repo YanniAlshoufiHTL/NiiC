f44c53c3adc98daead0c0b6c68ed2cc53f880787
iboLawand <l.ibo@HTBLALeonding.onmicrosoft.com>
2024-03-04 13:25:09 +0100

    modified the database

--- code/database/2024_02_29_create_tables.sql
+++ code/database/2024_02_29_create_tables.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE niicuser
 (
     id       SERIAL PRIMARY KEY,
@@ -7,7 +6,7 @@ CREATE TABLE niicuser
 
 CREATE TABLE calendar
 (
-    id       SERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     usernameid BIGINT NOT NULL REFERENCES niicuser
 );
 
@@ -20,15 +19,15 @@ CREATE TABLE aet
     type        varchar(40)  NOT NULL CHECK ( type = 'appointment' OR type = 'event' OR type = 'task' ),
     tsbegin     TIMESTAMP    NOT NULL,
     tsend       TIMESTAMP    NOT NULL,
-    color       VARCHAR(7) NOT NULL CHECK (color ~* '^#[a-f0-9]{6}$') DEFAULT '#23414b',
-    calenderid  BIGINT NOT NULL REFERENCES calendar
+    color       VARCHAR(7)   NOT NULL CHECK (color ~* '^#[a-f0-9]{6}$') DEFAULT '#23414b',
+    calenderid  BIGINT       NOT NULL REFERENCES calendar
 );
 
 CREATE TABLE blockmodule
 (
     id          SERIAL PRIMARY KEY,
-    token       VARCHAR(40) UNIQUE NOT NULL ,
-    title       VARCHAR(255) NOT NULL ,
+    token       VARCHAR(40)  NOT NULL UNIQUE,
+    title       VARCHAR(255) NOT NULL,
     description TEXT,
     html        TEXT,
     css         TEXT,
