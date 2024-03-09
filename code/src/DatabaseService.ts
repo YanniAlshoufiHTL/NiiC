d9c3e7046ef47062910fe4bfe6933589be1a95d6
Yanni Alshoufi <yanni@Yannis-MacBook-Pro.local>
2024-03-09 11:38:22 +0100

    Documented DatabaseService

--- code/src/DatabaseService.ts
+++ code/src/DatabaseService.ts
@@ -4,11 +4,33 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: __dirname + "/../vars/.env" });
 
+/**
+ * Service for interacting with the database.
+ * Contains methods for adding, removing, and updating AETs.
+ */
 export class DatabaseService {
+    /**
+     * The local client used for the database interactions.
+     * Should never be used directly, use the `client` method instead.
+     * @private
+     */
     private _client: Client | undefined = undefined;
+
+    /**
+     * The AETs in the database.
+     * Should only be used after invoking the `setAets` method and awaiting or ".then"-ing it.
+     * @private
+     */
     private _aets: NiicAet[] | undefined = undefined;
+
+    // region Singleton
+
     private static _instance: DatabaseService | undefined = undefined;
 
+    /**
+     * Returns the instance of the database service.
+     * This method must be invoked each time the service is used.
+     */
     public static instance() {
         if (this._instance === undefined) {
             this._instance = new DatabaseService();
@@ -19,6 +41,12 @@ export class DatabaseService {
 
     private constructor() {}
 
+    // endregion
+
+    /**
+     * Sets the `_aets` field to the current AETs in the database if not yet set.
+     * @private
+     */
     private async setAets() {
         if (this._aets === undefined) {
             const client = await this.client();
@@ -40,8 +68,19 @@ export class DatabaseService {
         }
     }
 
+    /**
+     * Adds an AET to the database and caches it locally.
+     * @param {number} calendarId The ID of the calendar to add the AET to.
+     * @param {string} title The title of the AET.
+     * @param {string} description The description of the AET.
+     * @param {Date} date The date at which the AET takes place.
+     * @param {number} startTime The time of the day (0 <= t <= 24) at which the AET starts.
+     * @param {number} endTime The time of the day (0 <= t <= 24) at which the AET ends.
+     * @param {"appointment" | "event" | "task"} type The type of the AET ("appointment", "event", or "task").
+     * @param {string} color The color of the AET as /^#[0-9a-f]{6}$/. (e.g. "#f303ed")
+     */
     public async addAet(
-        userid: number,
+        calendarId: number,
         title: string,
         description: string,
         date: Date,
@@ -59,7 +98,7 @@ export class DatabaseService {
                          FROM calendar
                          WHERE niicuserid = $8::bigint))
                 RETURNING id;
-            `, [title, description, date, startTime, endTime, type, color, userid]);
+            `, [title, description, date, startTime, endTime, type, color, calendarId]);
             const id = +res.rows[0].id;
             await this.setAets();
             this._aets!.push({
@@ -79,6 +118,10 @@ export class DatabaseService {
         }
     }
 
+    /**
+     * Removes an AET from the database and from local cache based on an ID provided.
+     * @param {number} id The ID of the AET to remove.
+     */
     public async removeAet(id: number) {
         try {
             const client = await this.client();
@@ -92,10 +135,10 @@ export class DatabaseService {
     }
 
     /**
-     * Update an AET in the database.
+     * Updates an AET in the database.
      * `id` field of aet is ignored.
-     * @param {number} id
-     * @param {NiicAet} aet
+     * @param {number} id The ID of the AET to update.
+     * @param {NiicAet} aet The AET to update. Its `id` field is ignored.
      */
     public async updateAet(id: number, aet: NiicAet) {
         try {
@@ -119,13 +162,20 @@ export class DatabaseService {
         }
     }
 
+    /**
+     * Prints all AETs in the database to the console.
+     * If the AETs are cached, it uses the cached version..
+     */
     public async printAets() {
         await this.setAets();
         console.log(this._aets);
     }
 
 
-
+    /**
+     * The local client used for the database interactions.
+     * @private
+     */
     private async client(): Promise<Client> {
         if (this._client === undefined) {
             this._client = new Client({
