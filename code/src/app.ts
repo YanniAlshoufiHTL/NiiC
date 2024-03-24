9d1e5f55e63d072116a5f3b299d8172e49d7f1d9
Yanni Alshoufi <yanni@Yannis-MacBook-Pro.local>
2024-03-24 20:23:53 +0100

    Added block module REST API endpoints

--- code/src/app.ts
+++ code/src/app.ts
@@ -5,6 +5,7 @@ import { calRouter } from "./routers/cal-router";
 import { aetRouter } from "./routers/aet-router";
 import { storeRouter } from "./routers/store-router";
 import { loginRouter } from "./routers/login-router";
+import { modulesRouter } from "./routers/modules-router";
 
 const app = express();
 app.use(express.json());
@@ -18,6 +19,7 @@ app.use("/api/users", usersRouter);
 app.use("/api/cals", calRouter);
 app.use("/api/aets", aetRouter);
 app.use("/api/store", storeRouter);
+app.use("/api/modules", modulesRouter);
 
 
 const port = 3000;
