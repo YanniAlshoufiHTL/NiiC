5d62ab4c818dfe4fa2375de8e481474f8baf4bc0
ClemensGugerbauer <c.gugerbauer@HTBLALeonding.onmicrosoft.com>
2024-04-07 21:39:36 +0200

    frontend token generation and typescript code pending

--- code/public/src/generateToken_http/generateToken_http.ts
+++ code/public/src/generateToken_http/generateToken_http.ts
@@ -1,5 +1,32 @@
 async function generateToken_http() {
+
+    const pluginType: HTMLInputElement | null = document.querySelector(".niic-profile-token-generation-kindOfPlugin-form");
+
+    const write: HTMLInputElement | null = document.querySelector("");
+    const read: HTMLInputElement | null = document.querySelector("");
+
+    const checkbox : HTMLInputElement | null = <HTMLInputElement> document.getElementById("overrideToken");
+    if(checkbox.checked) {
+        const oldToken: HTMLInputElement | null = document.querySelector(".niic-profile-token-generation-OverrideToken-OldToken-input");
+    }
+
     const response = await fetch(`/api/tokens/`, {
         method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body:
+            JSON.stringify({
+                "type": "blm",
+                "write": [5, 2, 10],
+                "read": [1, 59, 100],
+                "oldToken": "blm--R--W-12345"
+            })
     });
+
+    if(response.status !== 204){
+        alert(response.status);
+        alert(await response.text());
+        return;
+    }
 }
