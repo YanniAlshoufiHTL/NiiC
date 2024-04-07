7e7309828f0ac3ee7e5af8e2973efe14ddcc4a37
YanniAlshoufiHTL <y.al_shoufi@HTBLALeonding.onmicrosoft.com>
2024-04-07 13:12:53 +0200

    did HTMl refactors

---
+++ code/public/src/plugins-related/installedModulesSetup.ts
@@ -0,0 +1,27 @@
+onInstalledModulesLoaded(() => {
+    const pluginListing = document.createElement('niic-plugin-listing');
+
+    const storeMods = installedBlockModules
+        .map(mod => {
+            return {
+                id: mod.id,
+                title: mod.title ? mod.title : "No title",
+                description: mod.description  ? mod.description : "No description",
+                additionalText: [
+                    mod.html ? "HTML" : null,
+                    mod.css ? "CSS" : null,
+                    mod.js ? "JS" : null,
+                ]
+                    .filter(text => text !== null)
+                    .join(" + "),
+                type: mod.type,
+            };
+        })
+
+    pluginListing.setAttribute('data-mods', JSON.stringify(storeMods));
+    pluginListing.setAttribute('data-mode', 'uninstall');
+
+    document
+        .querySelector(".niic-plugin-store-content-container")
+        ?.appendChild(pluginListing);
+});
